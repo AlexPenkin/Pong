1e5dcf858253b8b91e6baaec4d3f6dae6aaec1a1
AlexPenkin <a.penkin@ts-soft.ru>
2017-05-12 16:38:22 +0300

    tslint installing try (need ts 2.2.1)

--- src/Game.ts
+++ src/Game.ts
@@ -5,14 +5,14 @@ export default class Game {
     private canvas: Canvas;
     private players: Player[] = [];
     constructor() {
-    this.init();
+     this.init();
     }
 
-    private init() {
-        this.canvas = new Canvas({
+            private init() {
+            this.canvas = new Canvas({
             selector: 'root'
         });
-        if( this.players.length < 2 && this.players.length == 0) {
+        if( this.players.length < 2 && this.players.length === 0) {
             this.players.push(new Player(this, 'real'));
         }
     }
